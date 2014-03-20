d9e3e450bc0f10b533df29ef92a6c9eee712068c
Espen Moe-Nilssen <espen@medialog.no>
2014-03-20 09:15:34 +0100

    removed build folder

--- build/lib/medialog/googlefonts/__init__.py
+++
@@ -1,9 +0,0 @@
-from Products.CMFCore.FSFile import FSFile
-from Products.CMFCore.DirectoryView import registerFileExtension
-from Products.CMFCore.DirectoryView import registerMetaType
-
-registerFileExtension('ttf', FSFile)
-registerFileExtension('svg', FSFile)
-registerFileExtension('woff', FSFile)
-registerFileExtension('eot', FSFile)
-registerMetaType('File', FSFile)
--- build/lib/medialog/googlefonts/browser/viewlet.py
+++
@@ -1,8 +0,0 @@
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from plone.app.layout.viewlets.common import ViewletBase
-from plone.memoize.instance import memoize
-from Acquisition import aq_inner
-
-
-class GooglefontsHead(ViewletBase):    
-    render = ViewPageTemplateFile('viewlet.pt')
