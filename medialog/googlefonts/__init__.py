e52196ebd08509695f52e2d7ef5e0b2b994ab6c8
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2018-06-21 14:20:05 -0400

    Enabled i18n support

--- medialog/googlefonts/__init__.py
+++ medialog/googlefonts/__init__.py
@@ -1,6 +1,9 @@
 from Products.CMFCore.FSFile import FSFile
 from Products.CMFCore.DirectoryView import registerFileExtension
 from Products.CMFCore.DirectoryView import registerMetaType
+from zope.i18nmessageid import MessageFactory
+
+messageFactory = MessageFactory('medialog.googlefonts')
 
 registerFileExtension('ttf', FSFile)
 registerFileExtension('svg', FSFile)
--- medialog/googlefonts/interfaces.py
+++ medialog/googlefonts/interfaces.py
@@ -1,6 +1,6 @@
 from zope.interface import Interface
 from zope import schema
-from zope.i18nmessageid import MessageFactory
+from medialog.googlefonts import messageFactory as _
 
 
 from z3c.form import interfaces
@@ -9,15 +9,13 @@ from plone.directives import form
 from medialog.controlpanel.interfaces import IMedialogControlpanelSettingsProvider
 
 
-_ = MessageFactory('medialog.googlefonts')
-
 class IGooglefontsLayer(Interface):
     """ marker interface for googlefont layer """
     
 
 class IGooglefontsSettings(form.Schema):
     """Adds googlefont settings to medialog.controlpanel """
-    
+
     form.fieldset('googlefonts',
                   label=_(u'Googlefont Settings'),
                   fields=[ 'googlefontfamily',
@@ -28,9 +26,9 @@ class IGooglefontsSettings(form.Schema):
                            ]
     )
 
-    googlefonts = schema.TextLine(title=_(u"googlefonts", default=u'Googlefonts on'), 
+    googlefonts = schema.TextLine(title=_(u"googlefonts", default=u'Google Font on'), 
         description=_(u"googlefonts_on",
-        default=u'The divs or classes to put the FIRST googlefont on.'),
+        default=u'The divs or classes to put the FIRST Google Fonts on.'),
         required=True,
     )
 
@@ -40,17 +38,17 @@ class IGooglefontsSettings(form.Schema):
         ) ,
         required=True,
         min_length=1,
-        title=_(u"label_googlefontfamily", default=u"Enble fonts"),
-        description=u"Select fonts. Preview of fonts at <a href='@@fontsheet' target='_blank' >http://yoursite.com/@@fontsheet</a>",
+        title=_(u"label_googlefontfamily", default=u"Enable fonts"),
+        description=_(u"help_googlefontfamily", default=u"Select fonts. Preview of fonts at <a href='@@fontsheet' target='_blank' >http://yoursite.com/@@fontsheet</a>"),
     )
    
-    googlefontfamilysize = schema.TextLine(title=_(u"label_googlefontfamily", default=u'Size of the font'),
-        description=_(u"help_googlefontfamilysizs", default=u'Font Size for FIRST googlefont'),
+    googlefontfamilysize = schema.TextLine(title=_(u"label_googlefontfamilysize", default=u'Size of the font'),
+        description=_(u"help_googlefontfamilysize", default=u'Font Size for FIRST googlefont'),
         required=False,
         default=_("300%"),
     )
 
-    googlefontcss = schema.Text(title=_(u"label_googlefontcss", default=u'CSS for FIRST googlefont. No typos please....'),
+    googlefontcss = schema.Text(title=_(u"label_googlefontcss", default=u'CSS for FIRST Google Font. No typos please....'),
         description=_(u"help_googlefontcss", default=u''),
         required=False,
     )
@@ -58,7 +56,7 @@ class IGooglefontsSettings(form.Schema):
     extracss = schema.Text(title=_(u"label_extracss", 
      default=u'Extra css. Include classes and divs. No typos please....'),
                            description=_(u"help_extracss",
-                                         default=u'Example: #content .sometitle { font-family: "My Font", "Times", "Sans-Serif" !important; font-size: 300% !important;'),
+                                         default=u"Example: #content .sometitle { font-family: \"My Font\", 'Times', 'Sans-Serif' !important; font-size: 300% !important; }"),
         required=False,
     )
 
@@ -72,4 +70,3 @@ alsoProvides(IGooglefontsSettings, IMedialogControlpanelSettingsProvider)
 
 class IGooglefontsLayer(Interface):
     """ marker interface for googlefont layer """
-    
