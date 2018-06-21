21cb66dcb632dbeec52fc9d4526ed5dce20c2cb0
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2018-06-21 14:33:46 -0400

    Updated some values and added others package metadata items

--- setup.py
+++ setup.py
@@ -1,19 +1,33 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.2.2'
+version = '2.2.3'
 
 setup(name='medialog.googlefonts',
       version=version,
       description="Googlefonts for Plone",
       long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      # Get more strings from https://pypi.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.3',
+        'Framework :: Plone :: Theme',
+        'Framework :: Zope2',
+        'Framework :: Zope3',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         ],
-      keywords='',
+      keywords='web zope plone cms google fonts library',
       author='Espen Moe-Nilssen',
       author_email='espen@medialog.no',
       url='http://medialog.no',
